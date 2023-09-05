98a14f1a5b83dc86a843195922418114e4dfd3ff
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-05 18:40:37 +0300

    create pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+
+const App = () => {
+  return <div>App</div>;
 };
+
+export default App;
