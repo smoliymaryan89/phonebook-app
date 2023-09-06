2d1fb4f9d575e92a4074a771737e7695bd185e1c
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-06 21:15:21 +0300

    styled HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box
+      bg="white"
+      shadow="xl"
+      w="600px"
+      p={30}
+      position={'absolute'}
+      top={'50%'}
+      left={'50%'}
+      transform="translate(-50%, 50%)"
+    >
+      <Heading as="h1" fontSize={27} fontWeight={700} mb={6}>
+        Welcome to PhoneBook, your one-stop destination for all your contact
+        management needs!
+      </Heading>
+      <Text fontSize={18}>
+        Are you tired of sifting through messy address books, struggling to find
+        the right contact when you need it most? Look no further! PhoneBook is
+        here to simplify your life and help you effortlessly organize and access
+        your contacts.
+      </Text>
+    </Box>
+  );
 };
 
 export default HomePage;
