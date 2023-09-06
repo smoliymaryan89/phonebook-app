ae957eeded8f81bac4bc6f2e60d05f7a3280462b
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-06 13:25:37 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,6 +5,28 @@ import { selectContacts } from 'redux/contacts/contactsSelectors';
 import { useFormik } from 'formik';
 import { addContact } from 'redux/contacts/contactsOperations';
 
+const validationSchema = yup.object({
+  name: yup
+    .string()
+    .required('Required!')
+    .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/, {
+      message:
+        "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
+      excludeEmptyString: true,
+    }),
+  number: yup
+    .string()
+    .required('Required!')
+    .matches(
+      /^\+?\d{1,4}[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/,
+      {
+        message:
+          'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
+        excludeEmptyString: true,
+      }
+    ),
+});
+
 const ContactForm = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
@@ -14,27 +36,7 @@ const ContactForm = () => {
       name: '',
       number: '',
     },
-    validationSchema: yup.object({
-      name: yup
-        .string()
-        .required('Required!')
-        .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/, {
-          message:
-            "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
-          excludeEmptyString: true,
-        }),
-      number: yup
-        .string()
-        .required('Required!')
-        .matches(
-          /^\+?\d{1,4}[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/,
-          {
-            message:
-              'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
-            excludeEmptyString: true,
-          }
-        ),
-    }),
+    validationSchema,
     onSubmit: data => {
       const isDuplicate = contacts.find(contact => contact.name === data.name);
       if (isDuplicate) return alert(`${data.name} is already in contacts.`);
