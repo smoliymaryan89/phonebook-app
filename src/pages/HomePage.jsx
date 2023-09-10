19e2ef120e4ae4219262e222ba374f033c9cbbb4
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-09-10 20:58:43 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,21 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 
 const HomePage = () => {
   return (
-    <Box
-      bg="white"
-      shadow="xl"
-      w="600px"
-      p={30}
-      position={'absolute'}
-      top={'50%'}
-      left={'50%'}
-      transform="translate(-50%, 50%)"
-    >
-      <Heading as="h1" fontSize={27} fontWeight={700} mb={6}>
-        Welcome to PhoneBook, your one-stop destination for all your contact
-        management needs!
-      </Heading>
-      <Text fontSize={18}>
-        Are you tired of sifting through messy address books, struggling to find
-        the right contact when you need it most? Look no further! PhoneBook is
-        here to simplify your life and help you effortlessly organize and access
-        your contacts.
-      </Text>
-    </Box>
+    <Flex align="center" justify="center" h="75vh">
+      <Box bg="white" shadow="xl" w="600px" p={30}>
+        <Heading as="h1" fontSize={27} fontWeight={700} mb={6}>
+          Welcome to PhoneBook, your one-stop destination for all your contact
+          management needs!
+        </Heading>
+        <Text fontSize={18}>
+          Are you tired of sifting through messy address books, struggling to
+          find the right contact when you need it most? Look no further!
+          PhoneBook is here to simplify your life and help you effortlessly
+          organize and access your contacts.
+        </Text>
+      </Box>
+    </Flex>
   );
 };
 
